28e01a7f1499e636810d6147853478444e73468d
Kuvam <kuvamdevgan@gmail.com>
2019-10-30 02:37:15 +0530

    hello world in c

---
+++ scripts/c_hello_world_kd.c
@@ -0,0 +1,11 @@
+// LANGUAGE: C
+// AUTHOR: KD
+// GITHUB: https://github.com/phoenix-kd
+
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
